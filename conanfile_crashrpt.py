4e1982a2db25736e2674d1f2edef85dfa8fa8eee
Francisco Puerta <fpuerta@werfen.com>
2025-08-14 14:38:41 +0200

    Add crashrpt library

---
+++ conanfile_crashrpt.py
@@ -0,0 +1,21 @@
+from conans import ConanFile, tools
+
+class CrashRptConan(ConanFile):
+	name = "CrashRpt"
+	version = "1.4.3-5"
+	description = "C++ library to generate crash reports"
+	author = "CSW <csw@werfen.com>"
+	topics = ("conan", "crash", "report", "dump")
+	license = "BSD 3-Clause"
+	generators = "visual_studio"
+	settings = "os", "compiler", "build_type", "arch"
+
+	def package(self):
+		self.copy("CrashRpt.h", dst="include/crashrpt", src="include")
+		self.copy("CrashRpt1403%s.lib" % ("d" if self.settings.build_type == "Debug" else ""), dst="lib", src=("lib"))
+		self.copy("*.dll", dst="bin", src=("bin/Win32/%s/" % self.settings.build_type + "CrashRpt"))
+		self.copy("*.pdb", dst="bin", src=("bin/Win32/%s/" % self.settings.build_type + "CrashRpt"))
+
+	def package_info(self):
+		self.cpp_info.libs = tools.collect_libs(self)
+		self.cpp_info.bindirs = ['bin']
---
+++ conanfile_crashsender.py
@@ -0,0 +1,20 @@
+from conans import ConanFile, tools
+
+class CrashSenderConan(ConanFile):
+	name = "CrashSender"
+	version = "1.4.3-5"
+	description = "Application to send crash reports"
+	author = "CSW <csw@werfen.com>"
+	topics = ("conan", "crash", "report", "dump")
+	license = "BSD 3-Clause"
+	generators = "visual_studio"
+	settings = "os", "compiler", "build_type", "arch"
+
+	def package(self):
+		self.copy("crashrpt_lang.ini", dst="bin", src=("bin/Win32/%s/" % self.settings.build_type + "CrashSender"))
+		self.copy("*.exe", dst="bin", src=("bin/Win32/%s/" % self.settings.build_type + "CrashSender"))
+		self.copy("*.dll", dst="bin", src=("bin/Win32/%s/" % self.settings.build_type + "CrashRpt"))
+		self.copy("*.pdb", dst="pdb", src=("bin/Win32/%s/" % self.settings.build_type + "CrashSender"))
+
+	def package_info(self):
+		self.cpp_info.bindirs = ['bin']
